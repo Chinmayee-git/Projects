341e3f8c4639e725efc4f3fc33cee00be81cf1e0
chinmayee <kolhe.chinmayee@gmail.com>
2021-04-28 23:26:16 +0530

    Update B1.py

--- Section_B/B1.py
+++ Section_B/B1.py
@@ -1,10 +1,21 @@
+# taking inputs
+
 n = input('Enter the size of the array : ')
 arr = input('Enter the array (digits with space in between): ').split(' ')
 arr = [int(j) for j in arr]
 
+# making an array of indices of all the '1' inputs
+
 indices = [i for i in range(len(arr)) if arr[i] == 1]
+
+# if you see, logically, all the 1's would gather around the central '1'
+
 mid = round(len(indices) / 2) - 1
+
 swaps = 0
+
+# this loop will itterate through the array and count the total number of swaps 
+# required to get all the other '1' (except the mid) towards the mid
 
 for x in range(len(indices)):
     swaps += abs(indices[mid] - indices[x]) - abs(mid - x)
